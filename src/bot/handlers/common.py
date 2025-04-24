45f3b98a4c1420389c74be3261754a1bf0a592fb
Miraska <miraska.007@gmail.com>
2025-04-24 12:23:27 +0300

    Update common.py

--- src/bot/handlers/common.py
+++ src/bot/handlers/common.py
@@ -11,29 +11,29 @@ router = Router()
 async def cmd_start(message: Message, state: FSMContext):
     await state.clear()
     if is_admin(message) and not is_registered_employee(message):
-        await message.answer("Добро пожаловать! Вы администратор. Вы можете управлять сотрудниками, используя команды в кнопках. Вы можете нанимать и уволнять сотрудников")
+        await message.answer("Добро пожаловать! Вы администратор. Вы можете управлять сотрудниками, используя кнопки. Вы можете нанимать и увольнять сотрудников")
         await message.answer("Выберите действие:", reply_markup=get_admin_menu())
         await state.set_state("admin:choosing_action")
 
     elif is_registered_employee(message) and not is_admin(message):
         if get_registered_employee(message).role == "senior_manager":
-            await message.answer("Добро пожаловать! Вы зарегистрированный старший сотрудник.")
+            await message.answer("Добро пожаловать! Вы зарегистрированы как старший сотрудник")
 
         else:
-            await message.answer("Добро пожаловать! Вы зарегистрированный сотрудник. Начните смену с помощью команды /start_shift, а для закрытия смены /end_shift.")
+            await message.answer("Добро пожаловать! Вы зарегистрированы как сотрудник. Начните смену с помощью кнопки “Начать смену”, после открытия смены у вас будут доступны варианты работы с сменой, затем вы сможете её закрыть!")
 
     elif is_admin(message) and is_registered_employee(message):
         if get_registered_employee(message).role == "senior_manager":
-            await message.answer("Добро пожаловать! Вы администратор и зарегистрированный старший сотрудник. Вы можете управлять сотрудниками, используя команды в кнопках. Вы можете нанимать и уволнять сотрудников")
+            await message.answer("Добро пожаловать! Вы администратор и зарегистрирован, как старший сотрудник. Вы можете управлять сотрудниками, используя кнопки. Вы можете нанимать и увольнять сотрудников, а также работать с сменами")
             await message.answer("Выберите действие:", reply_markup=get_admin_menu())
             await state.set_state("admin:choosing_action")
         else:
-            await message.answer("Добро пожаловать! Вы администратор и зарегистрированный сотрудник. Вы можете управлять сотрудниками, используя команды в кнопках. Вы можете нанимать и уволнять сотрудников")
+            await message.answer("Добро пожаловать! Вы администратор и зарегистрированы, как сотрудник. Вы можете управлять сотрудниками, используя кнопки, а также работать с сменами")
             await message.answer("Выберите действие:", reply_markup=get_admin_menu())
             await state.set_state("admin:choosing_action")
     
     else:
-        await message.answer("Добро пожаловать! Зарегистрируйтесь попросив у админа или начните смену если вы зарегестрированы с помощью команды /start_shift, а для закрытия смены /end_shift.")
+        await message.answer("Добро пожаловать! Ваш ID не добавлен в пул сотрудников, просьба обратиться к администратору, чтобы получить необходимые права")
 
 @router.message(Command("cancel"))
 async def cancel_handler(message: Message, state: FSMContext):
